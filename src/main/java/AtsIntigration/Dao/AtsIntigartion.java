f5464863c1e2043686e8e3397d6a5b0d1ddefbaa
Ravikr123png <ravikumar4gsm@gmail.com>
2023-04-04 10:11:15 -0700

    This  is first Code;

---
+++ src/main/java/AtsIntigration/Dao/AtsIntigartion.java
@@ -0,0 +1,10 @@
+package AtsIntigration.Dao;
+
+import java.sql.SQLOutput;
+
+public class AtsIntigartion {
+    public static void main(String[] args){
+        System.out.println("ATS");
+    }
+
+}
